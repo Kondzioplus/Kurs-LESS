0ecbaa05b70cb3041369a3a7f1eb6993c50ebafd
Kondzioplus <111319052+Kondzioplus@users.noreply.github.com>
2023-10-11 14:46:38 +0200

    zmiana wysokosci za pomocą skryptu

--- js/custom.js
+++ js/custom.js
@@ -8,3 +8,14 @@ $(document).ready(function(){
         interval: 3000
     });
 });
+
+// Scroll dla naglowka
+$(window).bind('scroll', function(event) {
+	if($(window).scrollTop() > 20) {
+		$('.main-header').addClass('header-short');
+		$('.main-logo img').css('max-height', 28);
+	} else {
+		$('.main-header').removeClass('header-short');
+		$('.main-logo img').css('max-height', 42);
+	}
+});
